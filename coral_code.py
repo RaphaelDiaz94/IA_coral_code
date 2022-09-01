679f84aee9c3d7e3d1b856748e6b959d87f8092b
DiazRaphael94 <diazyarribas.raphael@gmail.com>
2022-09-01 17:00:39 +0200

    modif boucle if

--- coral_code.py
+++ coral_code.py
@@ -108,7 +108,7 @@ def main():
       cur.close()
       conn.close()
 
-    if (nb_car != 0):
+    if (type(nb_car) == "int"):
       nomelement = "Voiture"
       conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
       cur = conn.cursor()
@@ -117,7 +117,7 @@ def main():
       cur.close()
       conn.close()
 
-    if (nb_motorcycle != 0):
+    if (type(nb_motorcycle) == "int"):
       nomelement = "Deux roues"
       conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
       cur = conn.cursor()
@@ -126,7 +126,7 @@ def main():
       cur.close()
       conn.close()
 
-    if (nb_truck != 0):
+    if (type(nb_truck) == "int"):
       nomelement = "Camion"
       conn = psycopg2.connect("postgres://owshwcafnfsgsx:2b4cf5ade3fb7b2f25e3f1b66cd29d5a7e420fdd1d51b4c01df4b6086f1db630@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d5arg29ce13853", sslmode='require')
       cur = conn.cursor()
